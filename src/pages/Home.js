1828f46abdcdb1a4948a441c15bc1b58b7f17651
zaberraiyan <zaberraiyan@gmail.com>
2023-04-12 03:42:20 -0400

    changes on main button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,11 +10,12 @@ export default function Home(props) {
       <div className="hero-text">
         <h1>{props.title}</h1>
         <p> {props.text}</p>
-        <a href={props.url}  className={props.btnClass}>
+        <a href={props.url}  className="btn btn-two">
         {props.buttonText}
         </a>
       </div>
-    <div className="des-text">
+      <div className="maindisplay">
+        <div className="des-text">
         <h2> About US</h2>
         <h2> Maintech Flowers by the Bay</h2>
         <p> A national garden and premier horticultural attraction for local and international visitors,
@@ -23,10 +24,7 @@ export default function Home(props) {
             visitors with plants seldom seen in this part of 
             the world, ranging from species in cool, temperate climates to tropical 
             forests and habitats.</p>
-              <a className="aboutimg" href="true">
-              <img src={About} alt=""> 
-              </img>
-              </a> 
+              
       </div>
       <div className="businesshours">
           <h2 style={{paddingBottom: 10, color: "black"}}>Business Hours</h2>
